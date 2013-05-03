3c943801e007a27a6480a74e7b6b854dafcfb1d5
jlangr <jeff@langrsoft.com>
2013-05-03 16:47:48 -0600

    fix highlight

--- Soundex.h
+++ Soundex.h
@@ -1,10 +1,12 @@
 #ifndef Soundex_h
 #define Soundex_h
 
+// START:nil
 class Soundex {
 public:
    std::string encode(const std::string& word) const {
       return nullptr;
    }
 };
+// END:nil
 #endif
