4851fe92258b7a029d6288853c1e48d8ea25b313
jlangr <jeff@langrsoft.com>
2013-05-04 15:40:01 -0600

    triangulation

--- Soundex.h
+++ Soundex.h
@@ -6,7 +6,7 @@ class Soundex {
 public:
    std::string encode(const std::string& word) const {
 // START_HIGHLIGHT
-      return "A000";
+      return word + "000";
 // END_HIGHLIGHT
    }
 };
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,14 +2,15 @@
 
 #include "Soundex.h"
 
-TEST_GROUP(ASoundexEncoding) {};
+TEST_GROUP(ASoundexEncoding) {
+   Soundex soundex;
+};
 
 // START:test
 TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
-   Soundex soundex;
-
-   auto encoded = soundex.encode("A");
-
-   CHECK_EQUAL("A000", encoded);
+   CHECK_EQUAL("A000", soundex.encode("A"));
+// START_HIGHLIGHT
+   CHECK_EQUAL("B000", soundex.encode("B"));
+// END_HIGHLIGHT
 }
 // END:test
