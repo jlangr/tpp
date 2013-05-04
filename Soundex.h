6969e8e28335f2b1fd1e906904c85fd720df950d
jlangr <jeff@langrsoft.com>
2013-05-04 17:14:45 -0600

    if

--- Soundex.h
+++ Soundex.h
@@ -6,7 +6,13 @@ class Soundex {
 public:
    std::string encode(const std::string& word) const {
 // START_HIGHLIGHT
-      return word + "000";
+      std::string code("");
+      code += word[0];
+      if (word[1])
+         code += "100";
+      else
+         code += "000";
+      return code;
 // END_HIGHLIGHT
    }
 };
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -6,11 +6,15 @@ TEST_GROUP(ASoundexEncoding) {
    Soundex soundex;
 };
 
-// START:test
 TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
 // START_HIGHLIGHT
    CHECK_EQUAL("B000", soundex.encode("B"));
 // END_HIGHLIGHT
+}
+
+// START:test
+TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+   CHECK_EQUAL("A100", soundex.encode("Ab"));
 }
 // END:test
