2bfb79a5236ad6438be60981370e9a06371cb8e7
jlangr <jeff@langrsoft.com>
2013-05-04 20:55:59 -0600

    fix highlight

--- Soundex.h
+++ Soundex.h
@@ -1,30 +1,24 @@
 #ifndef Soundex_h
 #define Soundex_h
 
-// START:impl
 class Soundex {
 public:
    std::string encode(const std::string& word) const {
       std::string code("");
-// START_HIGHLIGHT
       code += head(word) + encodeTail(word);
       return zeroPad(code);
-// END_HIGHLIGHT
    }
 
-// START_HIGHLIGHT
    char head(const std::string& word) const {
       return word[0];
    }
-// END_HIGHLIGHT
 
-// START_HIGHLIGHT
    std::string encodeTail(const std::string& word) const {
       if (word[1] != 0) return "1";
       return "";
    }
-// END_HIGHLIGHT
 
+// START:impl
    std::string zeroPad(const std::string& code) const {
 // START_HIGHLIGHT
       return code + (hasEncodedCharacters(code) ? "00" : "000");
@@ -36,6 +30,6 @@ public:
       return code[1] != 0;
    }
 // END_HIGHLIGHT
-};
 // END:impl
+};
 #endif
