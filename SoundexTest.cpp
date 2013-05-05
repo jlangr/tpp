38256483e539361a4a1a30915951b06773350b55
jlangr <jeff@langrsoft.com>
2013-05-04 20:49:56 -0600

    revert

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -8,14 +8,13 @@ TEST_GROUP(ASoundexEncoding) {
 
 TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
+// START_HIGHLIGHT
    CHECK_EQUAL("B000", soundex.encode("B"));
+// END_HIGHLIGHT
 }
 
 // START:test
 TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A100", soundex.encode("Ab"));
-// START_HIGHLIGHT
-   CHECK_EQUAL("A200", soundex.encode("Ac"));
-// END_HIGHLIGHT
 }
 // END:test
