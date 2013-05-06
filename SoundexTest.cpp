6e52182dd8d9cc6e0f7b00f7f15cb21dbbd8f565
jlangr <jeff@langrsoft.com>
2013-05-06 14:52:58 -0600

    fix name

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,17 +2,17 @@
 
 #include "Soundex.h"
 
-TEST_GROUP(ASoundexEncoding) {
+TEST_GROUP(SoundexEncoding) {
    Soundex soundex;
 };
 
-TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
+TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
    CHECK_EQUAL("B000", soundex.encode("B"));
 }
 
 // START:test
-TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A100", soundex.encode("Ab"));
 // START_HIGHLIGHT
    CHECK_EQUAL("A200", soundex.encode("Ac"));
