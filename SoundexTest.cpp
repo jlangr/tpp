8e4af7f8e369310455b9841d9bc1bb18537e8b6b
jlangr <jeff@langrsoft.com>
2013-05-06 14:57:37 -0600

    fix name

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,16 +2,16 @@
 
 #include "Soundex.h"
 
-TEST_GROUP(ASoundexEncoding) {
+TEST_GROUP(SoundexEncoding) {
    Soundex soundex;
 };
 
-TEST(ASoundexEncoding, AppendsZerosToWordForOneLetterWord) {
+TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
    CHECK_EQUAL("B000", soundex.encode("B"));
 }
 
-TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A100", soundex.encode("Ab"));
    CHECK_EQUAL("A200", soundex.encode("Ac"));
    CHECK_EQUAL("A300", soundex.encode("Ad"));
@@ -32,19 +32,19 @@ TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A200", soundex.encode("Az"));
 }
 
-TEST(ASoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+TEST(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
    CHECK_EQUAL("A234", soundex.encode("Acdl"));
 }
 
-TEST(ASoundexEncoding, IgnoresVowelLikeLetters) {
+TEST(SoundexEncoding, IgnoresVowelLikeLetters) {
    CHECK_EQUAL("B234", soundex.encode("BAaEeIiOoUuHhYycdl"));
 }
 
-TEST(ASoundexEncoding, IgnoresNonAlphabetics) {
+TEST(SoundexEncoding, IgnoresNonAlphabetics) {
    CHECK_EQUAL("F234", soundex.encode("F987654321%#.=+cdl"));
 }
 
-TEST(ASoundexEncoding, CombinesDuplicateEncodings) {
+TEST(SoundexEncoding, CombinesDuplicateEncodings) {
    CHECK_EQUAL(soundex.codeFor('f'), soundex.codeFor('b'));
    CHECK_EQUAL(soundex.codeFor('g'), soundex.codeFor('c'));
    CHECK_EQUAL(soundex.codeFor('t'), soundex.codeFor('d'));
@@ -52,29 +52,29 @@ TEST(ASoundexEncoding, CombinesDuplicateEncodings) {
    CHECK_EQUAL("A123", soundex.encode("Abfcgdt"));
 }
 
-TEST(ASoundexEncoding, CombinesMultipleDuplicateEncodings) {
+TEST(SoundexEncoding, CombinesMultipleDuplicateEncodings) {
    CHECK_EQUAL("J100", soundex.encode("Jbbb"));
 }
 
-TEST(ASoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+TEST(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
    CHECK_EQUAL("B230", soundex.encode("bbcd"));
 }
 
-TEST(ASoundexEncoding, UppercasesFirstLetter) {
+TEST(SoundexEncoding, UppercasesFirstLetter) {
    CHECK_EQUAL("A", soundex.encode("abcd").substr(0, 1));
 }
 
 // START:test
-TEST(ASoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
+TEST(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
    CHECK_EQUAL(soundex.encode("BCDL"), soundex.encode("bcdl"));
 }
 // END:test
 
-TEST(ASoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
+TEST(SoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels) {
    CHECK_EQUAL("J110", soundex.encode("Jbob")); 
 }
 
-TEST(ASoundexEncoding, LimitsLengthToFourCharacters) {
+TEST(SoundexEncoding, LimitsLengthToFourCharacters) {
    CHECK_EQUAL(4u, soundex.encode("Dcdlb").length());
 }
 
