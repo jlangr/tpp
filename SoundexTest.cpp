50e4fe97e292643821d789626d0f23201e5c9095
jlangr <jeff@langrsoft.com>
2013-06-28 11:07:23 -0600

    remove extra asserts

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -14,22 +14,6 @@ TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
 TEST(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
    CHECK_EQUAL("A100", soundex.encode("Ab"));
    CHECK_EQUAL("A200", soundex.encode("Ac"));
-   CHECK_EQUAL("A300", soundex.encode("Ad"));
-   CHECK_EQUAL("A100", soundex.encode("Af"));
-   CHECK_EQUAL("A200", soundex.encode("Ag"));
-   CHECK_EQUAL("A200", soundex.encode("Aj"));
-   CHECK_EQUAL("A200", soundex.encode("Ak"));
-   CHECK_EQUAL("A400", soundex.encode("Al"));
-   CHECK_EQUAL("A500", soundex.encode("Am"));
-   CHECK_EQUAL("A500", soundex.encode("An"));
-   CHECK_EQUAL("A100", soundex.encode("Ap"));
-   CHECK_EQUAL("A200", soundex.encode("Aq"));
-   CHECK_EQUAL("A600", soundex.encode("Ar"));
-   CHECK_EQUAL("A200", soundex.encode("As"));
-   CHECK_EQUAL("A300", soundex.encode("At"));
-   CHECK_EQUAL("A100", soundex.encode("Av"));
-   CHECK_EQUAL("A200", soundex.encode("Ax"));
-   CHECK_EQUAL("A200", soundex.encode("Az"));
 }
 
 TEST(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
